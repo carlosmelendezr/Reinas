f0bca82a13520f3b17ddfd8354a569527dc132ea
Carlos Melendez <carlosmelendezr@gmail.com>
2019-05-05 12:39:52 -0400

    Initial commit

--- app/src/main/java/com/carlosintranets/reinas/MainActivity.java
+++ app/src/main/java/com/carlosintranets/reinas/MainActivity.java
@@ -9,6 +9,7 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -21,12 +22,13 @@ public class MainActivity extends AppCompatActivity {
     private Bitmap bit;
     Tablero tab;
     ImageView mImageView;
+    ProblemaReinas p;
+    int x;
+    int y;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
 
         setContentView(R.layout.activity_main);
         // Get the application context
@@ -36,23 +38,56 @@ public class MainActivity extends AppCompatActivity {
         mResources = getResources();
 
         mImageView = (ImageView) findViewById(R.id.tablero);
-        Button mButton = (Button) findViewById(R.id.iniciar);
+        Button mIniciar = (Button) findViewById(R.id.iniciar);
+        Button mReIniciar = (Button) findViewById(R.id.reiniciar);
 
-        ProblemaReinas p = new ProblemaReinas();
-        tab = p.BuscarSolucion();
-
-        DibujarTablero dib = new DibujarTablero(tab);
-        bit = dib.Dibujar();
 
 
        // setContentView(new DibujarTablero(this,tab));
+        x=0;
+        y=0;
+        p = new ProblemaReinas();
 
-
-        mButton.setOnClickListener(new View.OnClickListener() {
+        tab = new Tablero();
+        MostrarTablero();
+        mIniciar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mImageView.setImageBitmap(bit);
+
+                tab = p.BuscarSolucion();
+                /*Celda celda = tab.proximaDisponible();
+                x=celda.getX();
+                y=celda.getY();
+                //Log.i("Reinas ","col="+x+",reinas="+y);
+
+                tab.setCeldaEstado(Tablero.CELDA_REINA,x,y);*/
+                MostrarTablero();
+                /*DibujarTablero dib = new DibujarTablero(tab);
+                bit = dib.Dibujar();
+
+                mImageView.setImageBitmap(bit);*/
+
             }
         });
+
+        mReIniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                tab = new Tablero();
+                MostrarTablero();
+            }
+        });
+
+
+
+    }
+
+    public void MostrarTablero() {
+        DibujarTablero dib = new DibujarTablero(tab);
+        bit = dib.Dibujar();
+
+        mImageView.setImageBitmap(bit);
+
     }
 }
