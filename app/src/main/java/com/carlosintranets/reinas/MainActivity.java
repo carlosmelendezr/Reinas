7ba5feb81163568345e0860c3bcc932f5253d849
Carlos Melendez <carlosmelendezr@gmail.com>
2019-05-05 17:42:03 -0400

    Initial commit

--- app/src/main/java/com/carlosintranets/reinas/MainActivity.java
+++ app/src/main/java/com/carlosintranets/reinas/MainActivity.java
@@ -6,29 +6,38 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Context mContext;
     private Resources mResources;
     private RelativeLayout mRelativeLayout;
     private Bitmap bit;
+    private int widthPixels;
+    private int heightPixels;
     Tablero tab;
     ImageView mImageView;
     ProblemaReinas p;
+    TextView estado;
     int x;
     int y;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
 
         setContentView(R.layout.activity_main);
         // Get the application context
@@ -40,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         mImageView = (ImageView) findViewById(R.id.tablero);
         Button mIniciar = (Button) findViewById(R.id.iniciar);
         Button mReIniciar = (Button) findViewById(R.id.reiniciar);
-
+        estado = findViewById(R.id.estado);
 
 
        // setContentView(new DibujarTablero(this,tab));
@@ -55,6 +64,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 tab = p.BuscarSolucion();
+                if (p.solucion) {
+                    estado.setBackgroundColor(Color.GREEN);
+                    estado.setText("Solucion encontrada!!!");
+                } else {
+                    estado.setBackgroundColor(Color.RED);
+                    estado.setText("Solucion NO encontrada");
+
+                }
                 /*Celda celda = tab.proximaDisponible();
                 x=celda.getX();
                 y=celda.getY();
@@ -84,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void MostrarTablero() {
-        DibujarTablero dib = new DibujarTablero(tab);
+        DibujarTablero dib = new DibujarTablero(this,tab);
         bit = dib.Dibujar();
 
         mImageView.setImageBitmap(bit);
